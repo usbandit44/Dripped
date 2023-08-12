c33c28db230ebcc0c2ec24ec68c1ca394ec0d129
usbandit44 <usbandit44@gmail.com>
2023-08-12 02:55:46 -0700

    reeact router setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import AddItem from "./front-end/pages/AddItem";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
+import Root from "./front-end/Root";
+import Closet from "./front-end/pages/Closet";
+import CreateOutfit from "./front-end/pages/CreateOutfit";
 
 function App() {
   return (
-    <>
-      <AddItem />;
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route path="" element={<Closet />} />
+          <Route path="create" element={<CreateOutfit />} />
+          <Route path="add" element={<AddItem />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/front-end/pages/CreateOutfit.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateOutfit = () => {
+  return <div>CreateOutfit</div>;
+};
+
+export default CreateOutfit;
